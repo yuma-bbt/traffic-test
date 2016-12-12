0db34d6e1bf3f2455078873154d51618b22bea61
yuma iwamoto <yuma@lab.bbtower.net>
2016-12-12 10:16:33 +0000

    fixed files

--- pkt-gen_test.sh
+++ pkt-gen_test.sh
@@ -1,3 +1,4 @@
+test_date=`date "+%Y%m%d_%H%M%S"`
 for i in 64 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500 
 do
 echo "rem_device_all"            > /proc/net/pktgen/kpktgend_0
@@ -12,7 +13,7 @@ echo "src_mac 08:00:27:4c:e6:6f" > /proc/net/pktgen/eth1
 echo "dst 192.168.31.101"              > /proc/net/pktgen/eth1
 echo "dst_mac 08:00:27:c6:66:16" > /proc/net/pktgen/eth1
 echo "start"                     > /proc/net/pktgen/pgctrl
-echo -n $i   |tee -a .
-tail  /proc/net/pktgen/eth1 -n 1 |tee -a .
+echo -n $i   |tee -a result_traffic_test$test_date
+tail  /proc/net/pktgen/eth1 -n 1 |tee -a result_traffic_test$test_date
 sleep 10
 done
