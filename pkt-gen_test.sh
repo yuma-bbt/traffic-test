e3a9680e7385cd67d1a5ade53c41102768deb38e
yuma iwamoto <yuma@lab.bbtower.net>
2016-12-12 09:13:56 +0000

    first commit

---
+++ pkt-gen_test.sh
@@ -0,0 +1,18 @@
+for i in 64 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500 
+do
+echo "rem_device_all"            > /proc/net/pktgen/kpktgend_0
+echo "add_device eth1"           > /proc/net/pktgen/kpktgend_0
+echo "count 10000"               > /proc/net/pktgen/eth1
+echo "clone_skb 1"               > /proc/net/pktgen/eth1
+echo "pkt_size 64"               > /proc/net/pktgen/eth1
+echo "delay 0"                   > /proc/net/pktgen/eth1
+echo "src_min 192.168.31.110"          > /proc/net/pktgen/eth1
+echo "src_max 192.168.31.110"          > /proc/net/pktgen/eth1
+echo "src_mac 08:00:27:4c:e6:6f" > /proc/net/pktgen/eth1
+echo "dst 192.168.31.101"              > /proc/net/pktgen/eth1
+echo "dst_mac 08:00:27:c6:66:16" > /proc/net/pktgen/eth1
+echo "start"                     > /proc/net/pktgen/pgctrl
+echo -n $i   |tee -a  /home/vagrant/result_pktgen
+tail  /proc/net/pktgen/eth1 -n 1 |tee -a /home/vagrant/result_pktgen
+sleep 10
+done
---
+++ plot.sh
@@ -0,0 +1,1 @@
+awk '{print $1,"\t",$3}' result_pktgen | gnuploter.sh
