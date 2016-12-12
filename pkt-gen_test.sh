b64d7b8da1f231842c867f6e369ab88e9f376f87
yuma iwamoto <yuma@lab.bbtower.net>
2016-12-12 20:38:37 +0000

    fixed files

--- pkt-gen_test.sh
+++ pkt-gen_test.sh
@@ -1,4 +1,5 @@
 test_date=`date "+%Y%m%d_%H%M%S"`
+touch data_store/result_traffic_test$test_date
 for i in 64 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500 
 do
 echo "rem_device_all"            > /proc/net/pktgen/kpktgend_0
@@ -13,8 +14,10 @@ echo "src_mac 08:00:27:4c:e6:6f" > /proc/net/pktgen/eth1
 echo "dst 192.168.31.101"              > /proc/net/pktgen/eth1
 echo "dst_mac 08:00:27:c6:66:16" > /proc/net/pktgen/eth1
 echo "start"                     > /proc/net/pktgen/pgctrl
-echo -n $i   |tee -a result_traffic_test$test_date
+echo -n $i   |tee -a data_store/result_traffic_test$test_date
 tail  /proc/net/pktgen/eth1 -n 1 |tee -a data_store/result_traffic_test$test_date
-sleep 10
+sleep 5 
 done
+#awk '{print $1,"\t",$3}' data_store/result_traffic_test$test_date | ./gnuploter.sh  graph_store/bandwidth-graph$test_date.png
+#awk '{print $1,"\t",$2,"\t",$3}' data_store/result_traffic_test$test_date | ./gnuploter.sh  graph_store/bandwidth-graph$test_date.png
 awk '{print $1,"\t",$3}' data_store/result_traffic_test$test_date | ./gnuploter.sh  graph_store/bandwidth-graph$test_date.png
